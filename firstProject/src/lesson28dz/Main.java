b4eb71f4c33691cf2c3c39656109337b3a899b2e
nara2011 <inaraakhmedova@gmail.com>
2025-02-14 21:08:03 +0400

    29LESSON

--- firstProject/src/lesson28dz/Main.java
+++ firstProject/src/lesson28dz/Main.java
@@ -2,11 +2,11 @@ package lesson28dz;
 
 public class Main {
     public static void main(String[] args) {
-        Shape circle = new Circle(5);
+        Circle circle = new Circle(5);
         System.out.println("Площадь круга: " + circle.area());
         System.out.println("Периметр круга: " + circle.perimeter());
 
-        Shape rectangle = new Rectangle(4, 6);
+        Rectangle rectangle = new Rectangle(4, 6);
         System.out.println("Площадь прямоугольника: " + rectangle.area());
         System.out.println("Периметр прямоугольника: " + rectangle.perimeter());
     }
--- firstProject/src/lesson28dz2/Main.java
+++ firstProject/src/lesson28dz2/Main.java
@@ -1,11 +1,11 @@
 package lesson28dz2;
 public class Main {
     public static void main(String[] args) {
-        Vehicle car = new Car();
+        Car car = new Car();
         car.start();
         car.stop();
 
-        Vehicle bicycle = new Bicycle();
+        Bicycle bicycle = new Bicycle();
         bicycle.start();
         bicycle.stop();
     }
